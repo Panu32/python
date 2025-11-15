d4ee5278b4d52603c1bd474139b27211b6d83d08
Pranav Vaidya <pranavvaidya2005@gmail.com>
2025-11-15 19:26:23 +0530

    Strings

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-#Variables
+# Variables
 
-st_cnt= 1000
+st_cnt = 1000
 print(st_cnt)
+
+rating = 4.99
+is_public = False
+
+message = """
+   Hello John,
+     Its Pranav.....
+"""
+
+
+course  =  "Python"
+print(len(course))
+
+#STRINGS 
+
+print(course[0:3])
